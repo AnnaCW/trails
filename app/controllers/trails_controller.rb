75f8fe43f42b04e83636a1d7ff6a6074921854e9
Anna Weisbrodt <aweisbro@gmail.com>
2016-05-31 15:04:14 -0600

    add create for trails

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -3,4 +3,29 @@ class TrailsController < ApplicationController
   def index
     @trails = Trail.all
   end
+
+  def new
+    @trail = Trail.new
+  end
+
+  def create
+    @trail = Trail.new(trail_params)
+      if @trail.save
+        redirect_to @trail
+      else
+        render :new
+      end
+  end
+
+  def show
+    @trail = Trail.find(params[:id])
+  end
+
+
+private
+
+  def trail_params
+    params.require(:trail).permit(:name, :image_path, :city, :state)
+  end
+
 end
