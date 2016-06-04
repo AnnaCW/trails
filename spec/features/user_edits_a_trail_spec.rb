d15b8b46fb34b72d86fcba958a45fa824efd7f5a
Anna Weisbrodt <aweisbro@gmail.com>
2016-06-04 09:04:20 -0600

    feature test user edits trail passing

--- spec/features/user_edits_a_trail_spec.rb
+++ spec/features/user_edits_a_trail_spec.rb
@@ -1,17 +1,19 @@
-# require 'rails_helper'
-#
-# RSpec.feature "User edits a trail" do
-#   scenario "user sees updated name" do
-#
-#     trail = create(:trail)
-#
-#     visit trail_path(trail)
-#     click_on "Edit"
-#     fill_in "Name", with: "Stormy Mountain"
-#     click_on "Update"
-#
-#     expect(page).to have_content "Stormy Mountain"
-#     expect(page).to have_no_content "153"
-#
-#   end
-# end
+require 'rails_helper'
+
+RSpec.feature "User edits a trail" do
+  scenario "logged in user edits trail created by that user" do
+    user = User.create(username: "User88", password: "password", password_confirmation: "password", role: 0)
+    ApplicationController.any_instance.stubs(:current_user).returns(user)
+    trail = user.trails.new(name: "Storm Mountain Route 153", city: "Fort Collins", state: "CO")
+    trail.save
+
+    visit trail_path(trail)
+    click_on "Edit"
+    fill_in "Name", with: "Stormy Mountain"
+    click_on "Update"
+
+    expect(page).to have_content "Stormy Mountain"
+    expect(page).to have_no_content "153"
+
+  end
+end
