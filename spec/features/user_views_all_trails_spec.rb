3897fcd0656562d781af29224f79349b4139fdaf
Anna Weisbrodt <aweisbro@gmail.com>
2016-06-04 09:07:26 -0600

    complete update of user-trails functionality

--- spec/features/user_views_all_trails_spec.rb
+++ spec/features/user_views_all_trails_spec.rb
@@ -1,16 +1,16 @@
-# require 'rails_helper'
-#
-# RSpec.feature "User views all trails" do
-#   scenario "user sees each trail name linked to trail page" do
-#
-#     user = User.create(username: "helloworld", password: "password", password_confirmation: "password")
-#     trails = user.trails.create(:trail, 3)
-#
-#     visit trails_path
-#
-#     trails.each do |trail|
-#       expect(page).to have_link trail.name, href: trail_path(trail)
-#     end
-#
-#   end
-# end
+require 'rails_helper'
+
+RSpec.feature "User views all trails" do
+  scenario "user sees each trail name linked to trail page" do
+
+    user = User.create(username: "helloworld", password: "password", password_confirmation: "password")
+    trails = create_list(:trail, 3)
+
+    visit trails_path
+
+    trails.each do |trail|
+      expect(page).to have_link trail.name, href: trail_path(trail)
+    end
+
+  end
+end
